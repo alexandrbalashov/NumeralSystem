8d70867ef946a901c6122f3accb497cccbc13aa0
alexandrbalashov <abalashow2002@mail.ru>
2015-04-17 15:02:55 +0400

    Написали функцию определяющую является ли символ цифрой. Написали функцию переводящую символ в число.

--- NumeralSystem.pas
+++ NumeralSystem.pas
@@ -1,1 +1,34 @@
-//пппп
+function isDigit(c:char):boolean;
+var cod:word;
+begin
+  cod:=OrdUnicode(c);
+  if (cod >= 48) and (cod <= 57) 
+    then isDigit:=true
+    else isDigit:=false;
+end;    
+
+function toDigit(c:char):shortint;
+var cod:word;
+begin
+  if (not isDigit(c)) then begin 
+    writeln('Ошибка в функции toDigit. Полученный символ не является цифрой');
+    toDigit:=-1;
+    exit;
+  end;
+  cod:=OrdUnicode(c);
+  toDigit:=cod-48;
+end;  
+
+
+
+var i:integer;
+    s:string;
+    c:char;
+    cod:word;
+begin
+  readln(s);
+  for i:=1 to length(s) do begin
+    writeln(toDigit(s[i]));
+  end;
+ 
+end.    
